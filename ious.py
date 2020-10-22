e6d4b668ecb4b44b32066fd53186ab10efb9f0ad
KoapT <632649396@qq.com>
2020-10-22 15:40:13 +0800

    ciou bug fixed

--- ious.py
+++ ious.py
@@ -13,11 +13,12 @@ import tensorflow as tf
 import numpy as np
 
 
-def bbox_iou(boxes1, boxes2, method='iou'):
+def bbox_iou(boxes1, boxes2, method='iou', beta=0.6):
     '''
     :param boxes1:  box: x(center)y(center)wh
     :param boxes2:  box: x(center)y(center)wh
     :param method: optional:'iou'/'giou'/'diou'/'ciou'
+    :param beta:   for diou
     :return:  ndims-1
     '''
 
@@ -49,24 +50,28 @@ def bbox_iou(boxes1, boxes2, method='iou'):
         return giou
     distance_enclose = tf.reduce_sum(enclose ** 2, axis=-1)
     distance_center = tf.reduce_sum((boxes1[..., :2] - boxes2[..., :2]) ** 2, axis=-1)
-    diou = iou - distance_center / (distance_enclose + 1e-10)
+    diou = iou - tf.pow(distance_center / (distance_enclose + 1e-10), beta)
     diou = tf.clip_by_value(diou, -1., 1.)
     if method == 'diou':
         return diou
-    v = 4 / (np.pi ** 2) * tf.square(tf.subtract(tf.atan(boxes1[..., 2] / boxes1[..., 3]),
-                                                 tf.atan(boxes2[..., 2] / boxes2[..., 3])))
-    ciou = diou - v ** 2 / (1 - iou + v)
+    v = 4 / (np.pi ** 2) * tf.square(tf.subtract(tf.atan(boxes1[..., 2] / (boxes1[..., 3] + 1e-10)),
+                                                 tf.atan(boxes2[..., 2] / (boxes2[..., 3] + 1e-10))))
+    S = tf.cast((iou > 0.4), tf.float64)
+    alpha = S * v / (1 - iou + v + 1e-10)
+    alpha_stoped = tf.stop_gradient(alpha)
+    ciou = iou - distance_center / (distance_enclose + 1e-10) - alpha_stoped * v
     ciou = tf.clip_by_value(ciou, -1., 1.)
     if method == 'ciou':
         return ciou
 
 
-def bbox_iou_np(boxes1, boxes2, method='iou'):
+def bbox_iou_np(boxes1, boxes2, method='iou', beta=0.6):
     '''
     Numpy implement of iou
     :param boxes1: box: x(center)y(center)wh
     :param boxes2: box: x(center)y(center)wh
     :param method: optional:'iou'/'giou'/'diou'/'ciou'
+     :param beta:   for diou
     :return:
     '''
     boxes1 = np.array(boxes1)
@@ -100,13 +105,33 @@ def bbox_iou_np(boxes1, boxes2, method='iou'):
         return giou
     distance_enclose = np.sum(enclose ** 2, axis=-1)
     distance_center = np.sum((boxes1[..., :2] - boxes2[..., :2]) ** 2, axis=-1)
-    diou = iou - distance_center / (distance_enclose + 1e-10)
+    diou = iou - np.power(distance_center / (distance_enclose + 1e-10), beta)
     diou = np.clip(diou, -1., 1.)
     if method == 'diou':
         return diou
-    v = 4 / (np.pi ** 2) * np.square(np.subtract(np.arctan(boxes1[..., 2] / boxes1[..., 3]),
-                                                 np.arctan(boxes2[..., 2] / boxes2[..., 3])))
-    ciou = diou - v ** 2 / (1 - iou + v)
+    v = 4 / (np.pi ** 2) * np.square(np.subtract(np.arctan(boxes1[..., 2] / (boxes1[..., 3] + 1e-10)),
+                                                 np.arctan(boxes2[..., 2] / (boxes2[..., 3] + 1e-10))))
+    ciou = iou - distance_center / (distance_enclose + 1e-10) - v ** 2 / (1 - iou + v + 1e-10)
     ciou = np.clip(ciou, -1., 1.)
     if method == 'ciou':
         return ciou
+
+
+# test
+if __name__=='__main__':
+    b1 = np.array([[1.,1.,3.,3.],[2.,2.,4.,4.],[0.,0.,0.,0.]])
+    b2 = np.array([[1.,2.,2,5],[4.,4.,4.,4.],[0.,0.,0.,0.]])
+
+    print(bbox_iou_np(b1,b2,method='iou'))
+    print(bbox_iou_np(b1,b2,method='giou'))
+    print(bbox_iou_np(b1,b2,method='diou',beta=1.))
+    print(bbox_iou_np(b1,b2,method='ciou'))
+
+    b1_tf = tf.Variable(b1)
+    b2_tf = tf.Variable(b2)
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        print(sess.run(bbox_iou(b1_tf,b2_tf,method='iou')))
+        print(sess.run(bbox_iou(b1_tf, b2_tf, method='giou')))
+        print(sess.run(bbox_iou(b1_tf, b2_tf, method='diou', beta=1.)))
+        print(sess.run(bbox_iou(b1_tf, b2_tf, method='ciou')))
